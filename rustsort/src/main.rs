531519dbff949510ef579de80c5678eac0a9aab4
John Deppe <deppe@ece.ubc.ca>
2017-11-22 13:04:39 -0800

    reads a file

--- rustsort/src/main.rs
+++ rustsort/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
