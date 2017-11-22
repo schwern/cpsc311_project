cde8e760c0b35aa4bb36ff8e57993fdfacbbd016
John Deppe <deppe@ece.ubc.ca>
2017-11-22 14:01:53 -0800

    change config to struct

--- rustsort/src/main.rs
+++ rustsort/src/main.rs
@@ -5,7 +5,7 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("In file {}", config.filename);
 
@@ -21,10 +21,13 @@ fn main() {
 struct Config {
     filename: String
 }
-fn parse_config(args: &[String]) -> Config {
-    let filename = args[1].clone();
-    // better to move than clone... how?
-    // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone
-    
-    Config {filename}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let filename = args[1].clone();
+        // better to move than clone... how?
+        // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone\
+        
+        Config {filename}
+    }
 }
