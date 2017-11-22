49c0ab3014f3fae59214d847ca6020e62e0d025e
John Deppe <deppe@ece.ubc.ca>
2017-11-22 13:05:06 -0800

    separate config

--- rustsort/src/main.rs
+++ rustsort/src/main.rs
@@ -5,15 +5,26 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[1];
+    let config = parse_config(&args);
 
-    println!("In file {}", filename);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
+    f.read_to_string(&mut contents) // replace this with a bufreader
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    filename: String
+}
+fn parse_config(args: &[String]) -> Config {
+    let filename = args[1].clone();
+    // better to move than clone... how?
+    // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone
+    
+    Config {filename}
 }
