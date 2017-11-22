0043ea134dc3c4bc1d7ee500485928a7878542f7
Maike <msgwyrdd@googlemail.com>
2017-11-22 14:58:22 -0800

    separated lib and main

--- rustsort/src/main.rs
+++ rustsort/src/main.rs
@@ -9,15 +9,10 @@ fn main() {
 
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents) // replace this with a bufreader
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    run(config);
 }
 
+<<<<<<< HEAD
 struct Config {
     filename: String
 }
@@ -30,4 +25,13 @@ impl Config {
         
         Config {filename}
     }
+=======
+
+fn parse_config(args: &[String]) -> Config {
+    let filename = args[1].clone();
+    // better to move than clone... how?
+    // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone
+
+    Config {filename}
+>>>>>>> separated lib and main
 }
