f9044f41fa4b114788a182ee771516967ba9d032
Maike <msgwyrdd@googlemail.com>
2017-11-23 15:18:56 -0800

    added struct and error handling

--- rustsort/src/lib.rs
+++ rustsort/src/lib.rs
@@ -1,61 +1,76 @@
 use std::error::Error;
 use std::fs:: File;
 use std::io::prelude::*;
+use std::cmp::Ordering;
+use std::io;
 
 pub struct Config {
-<<<<<<< HEAD
-    filename: String
-}
-
-fn run(config: Config) -> Result<(), Box<Error>>{
-    //read in file here, take info from Config struct
-    //then read line by line and collect lines in a vector
-    //call vector.sort() if Strings have total ordering
-    //"A mutable slice of elements with a total ordering has a sort method"
-    // https://stackoverflow.com/questions/26836488/how-to-sort-a-vector-in-rust
-
-
-
-}
-=======
     pub filename: String
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Config {
-        let filename = args[1].clone();
-        // better to move than clone... how?
-        // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone\
-        
-        Config {filename}
+        if(args.len() > 1){
+            let filename = args[1].clone();
+            Config {filename}
+        } else {
+            panic!("Not enough arguments given")
+        }
+
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>>{
+#[derive(Eq)]
+pub struct KeyLinePair<'a>{
+// key refers to a string slice of the line
+	key: &'a str,
+	line: String
+}
+
+impl<'a> PartialOrd for KeyLinePair<'a>{
+    fn partial_cmp(&self, other: &KeyLinePair) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> PartialEq for KeyLinePair<'a>{
+    fn eq(&self, other: &KeyLinePair) -> bool {
+        self.key == other.key
+    }
+}
+
+impl<'a> Ord for KeyLinePair<'a>{
+ fn cmp(&self, other: &KeyLinePair) -> Ordering {
+        self.key.cmp(&other.key)
+    }
+}
+
+pub fn run(config: Config) -> Result<(), io::Error>{
     //read in file here, take info from Config struct
-    let mut f = File::open(config.filename).expect("file not found");
+    let mut f = match File::open(config.filename){
+		Ok(file) => file,
+		Err(e) => return Err(e),
+	};
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
-    
+
     let mut nodes = Vec::new();
-    
+
     //then read line by line and collect lines in a vector
     for line in contents.lines() {
         nodes.push(line);
     }
-    
+
     //call vector.sort() if Strings have total ordering
     //"A mutable slice of elements with a total ordering has a sort method"
     // https://stackoverflow.com/questions/26836488/how-to-sort-a-vector-in-rust
     nodes.sort();
-    
+
     for node in nodes {
         println!("{}", node)
     }
 
     Ok(())
 }
-    
->>>>>>> ffc5e5132b1d388be6be5aa6710b6def755ce83d
--- rustsort/src/main.rs
+++ rustsort/src/main.rs
@@ -7,29 +7,9 @@ use rustsort::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-<<<<<<< HEAD
-    let config = parse_config(&args);
-
-    println!("In file {}", config.filename);
-
-    if let Err(e) = run(config){
-        println!("Application error: {}", e);
-
-        process:exit(1);
-    }
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let filename = args[1].clone();
-    // better to move than clone... how?
-    // https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html#the-tradeoffs-of-using-clone
-
-    Config {filename}
-=======
     let config = Config::new(&args);
 
     if let Err(e) = rustsort::run(config) {
-        println!{"Application error: {}", e};
+        panic!("Application error: {}", e)
     }
->>>>>>> ffc5e5132b1d388be6be5aa6710b6def755ce83d
 }
